b648471cbf26bf00ca19ffc5754bc88d2d2a11b6
swonbae <sbae13@my.centennialcollege.ca>
2021-08-20 00:17:26 -0400

    Added fireEvent for simulating user interaction

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,23 @@
 import * as React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import App from "./App";
 
 test("renders the correct content", () => {
   const { getByText, getByLabelText } = render(<App />);
 
-  // expect(getByText("TODOs")).not.toBeNull();
-  // expect(getByLabelText("What needs to be done?")).not.toBeNull();
-  // expect(getByText("Add #1")).not.toBeNull();
-
-  // => Still works well without assertion commented above
   expect(getByText("TODOs"));
   expect(getByLabelText("What needs to be done?"));
   expect(getByText("Add #1"));
+});
+
+test("allows users to add items to their list", () => {
+  const { getByText, getByLabelText } = render(<App />);
+
+  const input = getByLabelText("What needs to be done?");
+  fireEvent.change(input, { target: { value: "RTL Presentation Slides" } });
+  fireEvent.click(getByText("Add #1"));
+
+  getByText("RTL Presentation Slides");
+  getByText("Add #2");
 });
