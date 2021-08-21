8bdee6d015ceaea8fa561fd3f173c9af61339bf3
swonbae <sbae13@my.centennialcollege.ca>
2021-08-21 01:15:56 -0400

    Refactored using beforeAll()

--- src/App.test.js
+++ src/App.test.js
@@ -3,26 +3,25 @@ import { shallow } from "enzyme";
 import PersonList from "./PersonList";
 
 describe("App", () => {
-  it("renders without crashing", () => {
-    const appWrapper = shallow(<App />);
+  let appWrapper;
+
+  beforeAll(() => {
+    appWrapper = shallow(<App />);
   });
 
   it("renders a person list", () => {
-    const appWrapper = shallow(<App />);
     const personList = appWrapper.find(PersonList);
 
     expect(personList).toHaveLength(1);
   });
 
   it("has state", () => {
-    const appWrapper = shallow(<App />);
     const appState = appWrapper.state();
 
     expect(appState).not.toBeNull();
   });
 
   it("has a people property on state", () => {
-    const appWrapper = shallow(<App />);
     const appState = appWrapper.state();
 
     expect(appState.people).toBeDefined();
